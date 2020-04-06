7afd5f8570ac3faaaf3d08507f50074a766d7fe2
dannyhuang <kuanhsuh@gmail.com>
2020-04-07 07:43:57 +0800

    update 4.7

--- 18 getUserMedia/scripts.js
+++ 18 getUserMedia/scripts.js
@@ -7,12 +7,12 @@ const snap = document.querySelector(".snap");
 function getVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
-    .then(localMediaStream => {
+    .then((localMediaStream) => {
       video.srcObject = localMediaStream;
       console.log(video);
       video.play();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(`Oh NO!`, err);
     });
 }
@@ -28,7 +28,7 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
     // console.log(pixels);
-    pixels = rgbSplit(pixels);
+    // pixels = rgbSplit(pixels);
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
@@ -47,16 +47,6 @@ function takePhoto() {
   // link.textContent = "download image";
   link.innerHTML = `<img src=${data} alt="handsome" />`;
   strip.insertBefore(link, strip.firstChild);
-}
-
-function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 100;
-    pixels.data[i + 1] = pixels.data[i + 1] - 50;
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
-  }
-
-  return pixels;
 }
 
 function rgbSplit(pixels) {
